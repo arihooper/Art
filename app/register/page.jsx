02d3f15a8ededade6f41901eb2c52c3b9352085d
arihooper <arielsebsib@gmail.com>
2024-10-10 15:49:59 +0300

    adding feauters

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,16 +1,33 @@
-import "@Style/register.scss"
-
+import "@Style/Register.scss";
 
 const Register = () => {
-  return (
-    <div classname="register">
-        <img src="/assets/reg.jpg"/>
-        <div classname="register_content">
-            <form action=""></form>
-            <a href="/login">Got Account? LogIn Here</a>
+    return (
+        <div className="register">
+            <img src="/assests/reg.png" alt="Registering" />
+            <div className="register_content">
+                <form action="">
+                    <input placeholder="Username" name="username" required />
+                    <input placeholder="Email" name="email" required />
+                    <input placeholder="Password" name="password" required />
+                    <input placeholder="Confirm Password" name="confirmPassword" required />
+                    <input 
+                        id="image" 
+                        type="file" 
+                        name="profile" 
+                        accept="image/*" 
+                        style={{ display: "none" }} 
+                        required 
+                    />
+                    <label htmlFor="image">
+                        <img src="/assests/addpic.png" alt="Add Your Profile" />
+                        <p>Upload Photo</p>
+                    </label>
+                    <button type="submit">Register</button>
+                </form>
+                <a href="/login">Got an Account? Log In Here</a>
+            </div>
         </div>
-    </div>
-  )
+    );
 }
 
-export default Register
+export default Register;
